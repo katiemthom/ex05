32847c5230720e0f2f3a9a259c40f859ee65aaea
katiemthom <katiemthom@gmail.com>
2013-11-17 11:25:34 -0800

    update

--- lettertext.py
+++ lettertext.py
@@ -3,20 +3,13 @@ import sys
 #open the file
 #read it
 
-input_file = sys.argv[1]
-f = open(input_file)
+f = open(sys.argv[1])
 file_text = f.read()
 f.close()
 
 #create a list to keep the counts in 
 
 letter_counts = [0] * 26
-
-"""
-letter_dict = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7, 
-	"i": 8, "j": 9, "k": 10, "l": 11, "m": 12, "n": 13, "o": 14, "p": 15, "q": 16, 
-	"r": 17, "s": 18, "t": 19, "u": 20, "v": 21, "w": 22, "x": 23, "y": 24, "z": 25}
-"""
 
 #iterate through it one letter at a time
 # a = 0, b = 1, etc... to z = 25 
